00fe6c294e45b19f57dc2c1efa054d1b427c8434
Metro2033 <64359182+Metro2033-Max@users.noreply.github.com>
2021-11-12 11:05:57 +0800

    Update AppOn.java

--- src/main/java/com/gcode/app/AppOn.java
+++ src/main/java/com/gcode/app/AppOn.java
@@ -1,6 +1,6 @@
 package com.gcode.app;
 
-public class AppOn {
+public class AppOn {//edit by git
     public static void main(String[] args) {
         System.out.println("\"HELLO\" = " + "upt****upt by  git***");
     }
