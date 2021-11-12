921bbc3d12c71a3b3401f6186d10c330f9e5409d
shaozt <shaozt@126.com>
2021-11-12 10:34:07 +0800

    第一次提交

--- src/main/java/com/gcode/app/AppOn.java
+++ src/main/java/com/gcode/app/AppOn.java
@@ -2,6 +2,6 @@ package com.gcode.app;
 
 public class AppOn {
     public static void main(String[] args) {
-        System.out.println("\"HELLO\" = " + "HELLO");
+        System.out.println("\"HELLO\" = " + "upt*******");
     }
 }
