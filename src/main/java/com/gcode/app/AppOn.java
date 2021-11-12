e4b9745f1c8d8c4ab7736d8c9adba291032979f8
shaozt <shaozt@126.com>
2021-11-12 11:06:33 +0800

    冲突合并

--- src/main/java/com/gcode/app/AppOn.java
+++ src/main/java/com/gcode/app/AppOn.java
@@ -1,6 +1,6 @@
 package com.gcode.app;
 
-public class AppOn {
+public class AppOn {//edit by local
     public static void main(String[] args) {
         System.out.println("\"HELLO\" = " + "upt****upt by  git***");
     }
