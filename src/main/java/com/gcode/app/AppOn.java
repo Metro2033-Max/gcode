fdb82c8e1ba3523133d57ce5656b27be458b8e1f
shaozt <shaozt@126.com>
2022-02-17 11:53:18 +0800

    gitee 提交

--- src/main/java/com/gcode/app/AppOn.java
+++ src/main/java/com/gcode/app/AppOn.java
@@ -3,7 +3,7 @@ package com.gcode.app;
 public class AppOn {//edit by local,edit by git
 
     public static void main(String[] args) {
-        System.out.println("\"HELLO\" = " + "upt****upt by  git***");
+        System.out.println("\"HELLO\" = " + "upt****upt by  git commit 2 gitee***");
     }
 
     public static String getName() {
