68d0968caef8e7d6533d3c9ad36b460c2be7765d
shaozt <shaozt@126.com>
2022-02-17 14:25:51 +0800

    master修改

--- gitee/src/main/java/com/st/gitee/test/TestA.java
+++ gitee/src/main/java/com/st/gitee/test/TestA.java
@@ -3,6 +3,6 @@ package com.st.gitee.test;
 public class TestA {
     public static void main(String[] args) {
         System.out.println("args = " + args);
-        System.out.println("在dev分支上修改了。");
+        System.out.println("在master分支上修改了。");
     }
 }
