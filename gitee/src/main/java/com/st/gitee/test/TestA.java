93f5eafadcd2fae122376036087daf47caceb5de
shaozt <shaozt@126.com>
2022-02-17 14:13:33 +0800

    dev修改

---
+++ gitee/src/main/java/com/st/gitee/test/TestA.java
@@ -0,0 +1,8 @@
+package com.st.gitee.test;
+
+public class TestA {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+        System.out.println("在dev分支上修改了。");
+    }
+}
