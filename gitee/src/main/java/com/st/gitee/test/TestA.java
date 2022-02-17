fc44ab0394b498d1fad4d963eb39a2152dc36f21
shaozt <shaozt@126.com>
2022-02-17 16:44:05 +0800

    dev修改了文件

--- gitee/src/main/java/com/st/gitee/test/TestA.java
+++ gitee/src/main/java/com/st/gitee/test/TestA.java
@@ -4,5 +4,7 @@ public class TestA {
     public static void main(String[] args) {
         System.out.println("args = " + args);
         System.out.println("在dev分支上修改了。");
+        System.out.println("在dev分支上修改了。");
+        System.out.println("在dev分支上修改了。");
     }
 }
