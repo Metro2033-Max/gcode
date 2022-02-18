b6166ffd128ceacb129a751f214c64a7c4fb39d2
shaozt <shaozt@126.com>
2022-02-18 11:13:15 +0800

    master上修改了B

--- gitee/src/main/java/com/st/gitee/test/TestA.java
+++ gitee/src/main/java/com/st/gitee/test/TestA.java
@@ -4,6 +4,7 @@ public class TestA {
     public static void main(String[] args) {
         System.out.println("args = " + args);
         System.out.println("在master,dev分支上修改了。AAAAAAAAAAAAAA");
+        System.out.println("在master,dev分支上修改了。BBBBB");
         System.out.println("在dev分支上修改了。");
         System.out.println("在dev分支上修改了。");
     }
