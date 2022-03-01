c8bb3bbd8f47f7e9f771d7f370b47c4f429549fe
shaozt <shaozt@126.com>
2022-03-01 11:41:50 +0800

    新的分支测试rebase

--- gitee/src/main/java/com/st/gitee/test/NewFunction.java
+++ gitee/src/main/java/com/st/gitee/test/NewFunction.java
@@ -3,5 +3,6 @@ package com.st.gitee.test;
 public class NewFunction {
     public static void main(String[] args) {
         System.out.println("新功能新分支");
+        System.out.println("rebase测试");
     }
 }
